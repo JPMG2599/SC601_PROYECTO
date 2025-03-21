271ccc6e216749ddc1d76129c6482a219e75a4e4
Darrelus16 <darrel.cr16@gmail.com>
2025-03-20 21:18:56 -0600

    categoria
    categoria completo (agregar, consultar, modificar)

--- SC601_PRACTICA1-GRUPO5/SC601_V1/Controllers/CategoriaController.cs
+++ SC601_PRACTICA1-GRUPO5/SC601_V1/Controllers/CategoriaController.cs
@@ -60,7 +60,7 @@ namespace SC601_V1.Controllers
                     var result = context.SaveChanges();
 
                     if (result > 0)
-                        return RedirectToAction("AgregarCategoria", "Categoria");
+                        return RedirectToAction("ConsultarCategorias", "Categoria");
                     else
                     {
                         ViewBag.Mensaje = "La información no se ha podido registrar correctamente";
@@ -71,6 +71,54 @@ namespace SC601_V1.Controllers
             catch (Exception ex)
             {
                 error.RegistrarError(ex.Message, "Get AgregarCategoria");
+                return View("Error");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult ActualizarCategoria(long q)
+        {
+            try
+            {
+                using (var context = new KN_ProyectoEntities())
+                {
+                    var info = context.Categoria.Where(x => x.ID_Categoria == q).FirstOrDefault();
+                    return View(info);
+                }
+            }
+            catch (Exception ex)
+            {
+                error.RegistrarError(ex.Message, "Get ActualizarCategoria");
+                return View("Error");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ActualizarCategoria(Categoria model)
+        {
+            try
+            {
+                using (var context = new KN_ProyectoEntities())
+                {
+                    var info = context.Categoria.Where(x => x.ID_Categoria == model.ID_Categoria).FirstOrDefault();
+
+                    info.Nombre = model.Nombre;
+                    info.Descripcion = model.Descripcion;
+                    info.Imagen = model.Imagen;
+                    var result = context.SaveChanges();
+
+                    if (result > 0)
+                        return RedirectToAction("ConsultarCategorias", "Categoria");
+                    else
+                    {
+                        ViewBag.Mensaje = "La información no se ha podido actualizar correctamente";
+                        return View();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                error.RegistrarError(ex.Message, "Post ActualizarCategoria");
                 return View("Error");
             }
         }
